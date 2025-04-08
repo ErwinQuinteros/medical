0eaaa0374bbaab0e1efe86912bdedddde257252f
Erwin Quinteros <erwinquinterosvillarroel@gmail.com>
2025-04-07 22:44:09 -0300

    advanced nxt section

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,33 @@ function App() {
 
   return (
     <>
-    <div className='w-full mx-auto'>
-      <Navbar />
-      <Hero />
-    </div>
+      <div className='w-full mx-auto'>
+        <Navbar />
+        <Hero />
+        <div className="flex items-center justify-center w-full gap-8 px-6 py-10">
+          <div className="max-w-3xl mx-auto text-center">
+            <h2 className="text-4xl sm:text-5xl lg:text-6xl font-bold text-[#14457B] md:leading-20">
+              Nuestra especialidad es cuidarte
+            </h2>
+            <p className="mt-4 text-base sm:text-lg text-[#666666] mb-6">
+
+              En nuestra clínica ofrecemos una atención de calidad, enfocada en su bienestar y salud.
+              Contamos con un equipo médico altamente calificado, dispuesto a escuchar y comprender sus necesidades.
+              necesidades. Utilizamos equipos modernos y seguimos las mejores prácticas médicas.
+            </p>
+            <div className='flex items-center justify-center'>
+
+              <a
+                href="#inicio"
+                className="bg-[#19DDB8] text-white hover:bg-[#17c0a4] px-7 py-4 text-sm font-semibold rounded-full transition duration-300 uppercase text-center"
+              >
+                Saber más
+              </a>
+            </div>
+          </div>
+        </div>
+
+      </div>
     </>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,9 +4,9 @@ export const Hero = () => {
     return (
         <div>
             <div className="max-w-[1224px] mx-auto">
-                <div className="grid items-center w-full gap-8 px-6 py-10 lg:grid-cols-2 lg:gap-12 lg:py-20">
+                <div className="grid items-center w-full gap-8 px-6 py-10 lg:grid-cols-2 lg:gap-12 lg:py-28">
                     <div className="text-center lg:text-left">
-                        <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold text-[#14457B]">
+                        <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold text-[#14457B] md:leading-20">
                             Cuidamos de ti en cada detalle
                         </h1>
                         <p className="mt-4 text-base sm:text-lg text-[#666666] mb-6">
@@ -16,20 +16,20 @@ export const Hero = () => {
                         <div className="flex flex-col justify-center gap-4 sm:flex-row lg:justify-start">
                             <a
                                 href="#inicio"
-                                className="bg-[#19DDB8] text-white hover:bg-[#17c0a4] px-6 py-3 text-sm font-semibold rounded-full transition duration-300 uppercase text-center"
+                                className="bg-[#19DDB8] text-white hover:bg-[#17c0a4] px-7 py-4 text-sm font-semibold rounded-full transition duration-300 uppercase text-center"
                             >
                                 Agendar Consulta
                             </a>
                             <a
                                 href="#"
-                                className="bg-[#2490EB] text-white hover:bg-[#1e77c4] px-6 py-3 text-sm font-semibold rounded-full transition duration-300 uppercase text-center"
+                                className="bg-[#2490EB] text-white hover:bg-[#1e77c4] px-7 py-4 text-sm font-semibold rounded-full transition duration-300 uppercase text-center"
                             >
                                 Agendar Examen
                             </a>
                         </div>
                     </div>
 
-                    <div className="w-full px-6 sm:px-10 lg:px-0">
+                    <div className="w-full sm:px-10 lg:px-0">
                         <img
                             src="/images/banner_clinica.png"
                             alt="Banner Clínica"
